3bd0ff5b3e8cc4bcf4be4570bbda394117a242bb
eliaswnoel <eliaswnoel@gmail.com>
2024-04-15 16:12:33 -0400

    part4

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.urls import reverse
 
 # Create your models here.
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
+
 class Animal(models.Model):
     name = models.CharField(max_length=100)
     species = models.CharField(max_length=100)
@@ -14,3 +22,19 @@ class Animal(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'animal_id': self.id})
+
+class Feeding(models.Model):
+    date = models.DateField('feeding date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+        )
+    animal = models.ForeignKey(Animal, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
+    class Meta:
+        ordering = ['-date']
+
+    
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import FeedingForm
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Animal
 
 
 
 # Create your views here
+
+def add_feeding(request, animal_id):
+  # create a ModelForm instance using the data in request.POST
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # don't save the form to the db until it
+    # has the cat_id assigned
+    new_feeding = form.save(commit=False)
+    new_feeding.animal_id = animal_id
+    new_feeding.save()
+  return redirect('detail', animal_id=animal_id)
 
 def home(request):
   # Include an .html file extension - unlike when rendering EJS templates
@@ -19,9 +32,13 @@ def animals_index(request):
       'animals': animals
    })
 
-def animals_detail(request, animal_id): 
-    animal = Animal.objects.get(id=animal_id)
-    return render(request, 'animals/detail.html', {'animal': animal})
+def animals_detail(request, animal_id):
+  animal = Animal.objects.get(id=animal_id)
+  # instantiate FeedingForm to be rendered in the template
+  feeding_form = FeedingForm()
+  return render(request, 'animals/detail.html', {
+    'animal': animal, 'feeding_form': feeding_form
+  })
 
 class AnimalCreate(CreateView):
     model = Animal
